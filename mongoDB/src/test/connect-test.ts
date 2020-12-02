aa83e9470cd433ed8690c42914b7de36f87630c8
hangyeol0531 <jhan0531@naver.com>
2020-12-02 16:32:25 +0900

    ts-mongo

---
+++ mongoDB/src/test/connect-test.ts
@@ -0,0 +1,15 @@
+import {connect} from '../connect'
+
+const connectTest = async() =>{
+    let connection
+    try{
+        connection = await connect()
+        console.log('connection OK', connection)
+    }catch(e){
+        console.log(e.message)
+    }finally{
+        connection.close()
+    }
+}
+
+connectTest()
