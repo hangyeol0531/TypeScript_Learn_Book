b0cebd95583dc659edda5cbd330e45c863039088
hangyeol0531 <jhan0531@naver.com>
2020-12-04 12:41:38 +0900

    Invalid connection string

--- mongoDB/src/connect.ts
+++ mongoDB/src/connect.ts
@@ -1,4 +1,4 @@
 import MongoClient from 'mongodb'
 
-export const connect = (mongoUrl : string = 'mongodb://localhost:27017') =>
+export const connect = (mongoUrl : string = 'mongo://localhost:27017') =>
     MongoClient.connect(mongoUrl, {useNewUrlParser : true, useUnifiedTopology : true})
