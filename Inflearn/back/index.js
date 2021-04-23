a37f6e10b2608e43cf20e575c94b5e45e20423b9
hangyeol0531 <jhan0531@naver.com>
2021-04-24 07:08:19 +0900

    Ts-node

---
+++ Inflearn/back/index.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var prod = process.env.NODE_ENV === 'production';
+// productio 모드에서는 파일 캐싱, 에러 메세지 감추기
+// developlemnet 모드에서는 파일 캐시 방지, 디버깅 에러메세지를 보여준다.
+app.set('port', prod ? process.env.PORT : 3065);
+app.get('/', function (req, res, next) {
+    res.send('백엔드 정상 동작!');
+});
+// npx - gloabal 아닌 모듈을 실행
+app.listen(app.get('port'), function () {
+    console.log("server is running on " + process.env.PORT);
+});
--- Inflearn/back/index.ts
+++ Inflearn/back/index.ts
@@ -1,12 +1,17 @@
 import express = require('express');
+import {Request, Response, NextFunction} from 'express'
 
 const app = express();
-const prod = process.env.NODE_ENV === 'production'
+const prod : boolean = process.env.NODE_ENV === 'production'
+// productio 모드에서는 파일 캐싱, 에러 메세지 감추기
+// developlemnet 모드에서는 파일 캐시 방지, 디버깅 에러메세지를 보여준다.
 
-app.get('/', (req, res) =>{
+app.set('port', prod ? process.env.PORT : 3065)
+
+app.get('/', (req : Request, res : Response, next : NextFunction) =>{
     res.send('백엔드 정상 동작!');
 })
-
-app.listen(prod ? process.env.PORT : 3065, () =>{
+// npx - gloabal 아닌 모듈을 실행
+app.listen(app.get('port'), () =>{ //produection == null 아니면 dev 모드때는 3065 포트
     console.log(`server is running on ${process.env.PORT}`);
 })
