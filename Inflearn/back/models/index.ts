a0f144feb85c43b3dc2724ef76999574942a9eb9
hangyeol0531 <jhan0531@naver.com>
2021-04-24 11:37:22 +0900

    Sequelize

---
+++ Inflearn/back/models/index.ts
@@ -0,0 +1,12 @@
+import User, {associate as associateUser } from './user'
+export * from './sequelize'
+
+const db = {
+  User,
+};
+
+export type dbType = typeof db
+
+associateUser(db)
+
+// sequelize -> user -> index
