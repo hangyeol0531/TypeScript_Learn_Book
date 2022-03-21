1db75cb1f1d7386be6e60e94f2e434e9b6f80544
hangyeol0531 <jhan0531@naver.com>
2022-03-21 12:28:22 +0900

    tsconfig, type

---
+++ Captain_Basic/class-note/1_type-basic.ts
@@ -0,0 +1,10 @@
+// 문자열
+const str: string = 'hello';
+
+// 정수
+const num : numer = 10; 
+
+// 배열
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ['caption', 'Thor', 'Hulk'];
+const items: number[] = [1, 2, 3, 4];
