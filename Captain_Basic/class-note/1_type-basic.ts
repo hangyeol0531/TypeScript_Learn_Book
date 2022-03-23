f510ba17d265890e82e459b19bdf0ce54efdc6f2
hangyeol0531 <jhan0531@naver.com>
2022-03-23 23:57:07 +0900

    ts-basic

--- Captain_Basic/class-note/1_type-basic.ts
+++ Captain_Basic/class-note/1_type-basic.ts
@@ -2,9 +2,20 @@
 const str: string = 'hello';
 
 // 정수
-const num : numer = 10; 
+const num : number = 10; 
 
 // 배열
 const arr: Array<number> = [1, 2, 3];
 const heroes: Array<string> = ['caption', 'Thor', 'Hulk'];
 const items: number[] = [1, 2, 3, 4];
+
+// 튜플 각각 인덱스에 타입이 정의되어 있다.
+let address: [string, number] = ['gangname', 1];
+
+// 객체
+let obj: object = {};
+// 각 속성에 대해 타입 정의
+let person: {name: string, age: number} = {
+    name: 'capt',
+    age: 100,
+};
---
+++ Captain_Basic/class-note/2_functions.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number): number{
+    return a + b;
+}
+
+// js는 함수 파라미터 개수를 제한을 안하지만 ts는 제한 한다.
+// sum(10, 20, 30, 40, 50);
