a0dbbaeb2af7593027de83f214ee99861b3a9729
hangyeol0531 <jhan0531@naver.com>
2022-04-03 23:42:54 +0900

    utility type - partial, omit

--- Captain_Basic/class-note/13_utility-type.ts
+++ Captain_Basic/class-note/13_utility-type.ts
@@ -23,6 +23,58 @@ function fetchProducts(): Promise<Product[]>{
 
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 
+// pick과 반대로 옆 리스트 속성을 제외하는것
+type ShoppingItemOmit = Omit<Product, 'brand' | 'stock'>
+
+// 특정 상품의 상제 정보를 나타내기 위한 함수
 function displayProductDetail(shoppingItem: ShoppingItem){
 
+}
+
+// interface UpdateProduct{
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number; 
+// }
+
+// 전부 선택적으로
+type UpdateProduct = Partial<Product>
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+function updateProductItem(productItem : UpdateProduct){
+
+}
+
+// 4. 유틸리티 타입 구현 - Partial
+
+interface UserProfile{
+    username: string;
+    email: string;
+    productPhotoUrl: string;
+}
+
+// interface UserProfileUpdate{
+//     username?: string;
+//     email?: string;
+//     productPhotoUrl?: string;
+// }
+
+// #1
+// type UserProfileUpdate = {
+//     username?: UserProfile['username'];
+//     email?: UserProfile['email'];
+//     productPhotoUrl?: UserProfile['productPhotoUrl'];
+// }
+
+// #2
+// type UserProfileUpdate = {
+//     [p in 'username' | 'email' | 'productPhotoUrl']? : UserProfile[p]
+// }
+
+type UserProfileKeys = keyof UserProfile
+
+// #3
+type UserProfileUpdate<T> = {
+    [p in keyof T]? : T[p]
 }
