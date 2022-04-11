c68ea133f35f660d780525bfa9b9057a7fa0ca37
hangyeol0531 <jhan0531@naver.com>
2022-04-11 19:59:47 +0900

    Poi js Docs

--- Captain_Basic/project/app.js
+++ Captain_Basic/project/app.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 // utils
 function $(selector) {
   return document.querySelector(selector);
@@ -37,6 +39,14 @@ let isDeathLoading = false;
 let isRecoveredLoading = false;
 
 // api
+/**
+ * @typedef {object} CovidSummary
+ * @property {Array<object>}
+ */
+/**
+ * 
+ * @returns {Promise<CovidSummary>}
+ */
 function fetchCovidSummary() {
   const url = 'https://api.covid19api.com/summary';
   return axios.get(url);
